f23f3bf32cc98a2d8c6790fda7f130ac1d19a1b1
denisiodrigues <denisio@ymail.com>
2021-09-02 00:00:10 -0300

    Notificação e classe base de serviço

---
+++ src/Biblioteca.Business/Interfaces/INotificador.cs
@@ -0,0 +1,14 @@
+﻿using Biblioteca.Business.Notifications;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Biblioteca.Business.Interfaces
+{
+    public interface INotificador
+    {
+        bool TemNotificacao();
+        List<Notificacao> ObterNotificacoes();
+        void Handle(Notificacao notificacao);
+    }
+}
---
+++ src/Biblioteca.Business/Services/BaseService.cs
@@ -0,0 +1,45 @@
+﻿using Biblioteca.Business.Interfaces;
+using Biblioteca.Business.Models;
+using Biblioteca.Business.Notifications;
+using FluentValidation;
+using FluentValidation.Results;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Biblioteca.Business.Services
+{
+    public abstract class BaseService
+    {
+        private readonly INotificador _notificador;
+
+        public BaseService(INotificador notificador)
+        {
+            _notificador = notificador;
+        }
+
+        protected void Notificar(ValidationResult validationResult)
+        {
+            foreach (var erro in validationResult.Errors)
+            {
+                Notificar(erro.ErrorMessage);
+            }
+        }
+
+        protected void Notificar(string mensagem)
+        {
+            _notificador.Handle(new Notificacao(mensagem));
+        }
+
+        protected bool ExeutarValidaao<TV, TE>(TV validacao, TE entidade) where TV : AbstractValidator<TE> where TE: Entity
+        {
+            var validator = validacao.Validate(entidade);
+
+            if (validator.IsValid) return true;
+
+            Notificar(validator);
+
+            return true;
+        }
+    }
+}
