5a4abdc57eae42fc8974b15b908394620418d505
denisiodrigues <denisio@ymail.com>
2021-09-02 00:05:51 -0300

    Classes de validações de documento

---
+++ src/Biblioteca.Business/Validations/Doumentos/Utils.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Biblioteca.Business.Validations.Doumentos
+{
+    public class Utils
+    {
+        public static string ApenasNumeros(string valor)
+        {
+            var onlyNumber = "";
+            foreach (var s in valor)
+            {
+                if (char.IsDigit(s))
+                {
+                    onlyNumber += s;
+                }
+            }
+            return onlyNumber.Trim();
+        }
+    }
+}
